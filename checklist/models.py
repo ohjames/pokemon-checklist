31f83ed1c9b6d924444293e0ddd59c78557f37b6
James Nguyen <nguyenjames0404@gmail.com>
2023-03-03 10:06:19 -0800

    Added users to the website; need to add login/register page

--- checklist/models.py
+++ checklist/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Pokemon(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
     name = models.CharField(max_length=255)
     number = models.IntegerField()
     image = models.ImageField(upload_to='pokemon_images/')
--- checklist/views.py
+++ checklist/views.py
@@ -4,5 +4,17 @@ from .models import Pokemon
 
 # Create your views here.
 def pokemon_list(request):
+    if request.method == 'POST':
+        user_pokemon = Pokemon.objects.filter(user=request.user)
+        for pokemon in user_pokemon:
+            pokemon.user = None
+            pokemon.save()
+        for pokemon_id in request.POST.getlist('pokemon[]'):
+            try:
+                pokemon = Pokemon.objects.get(id=int(pokemon_id))
+                pokemon.user = request.user
+                pokemon.save()
+            except Pokemon.DoesNotExist:
+                pass
     pokemon = Pokemon.objects.all()
     return render(request, 'checklist/pokemon_list.html', {'pokemon': pokemon})
