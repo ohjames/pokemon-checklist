0f27044560feefccb67272b2f42fa6431b0378f0
James Nguyen <nguyenjames0404@gmail.com>
2023-03-15 13:31:28 -0700

    Models is correct; working on fixing everything else

--- checklist/models.py
+++ checklist/models.py
@@ -1,14 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 import uuid
 
 # Create your models here.
-class Pokemon(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+class Pokemon(models.Model): # Represents single Pokemon in checklist
     name = models.CharField(max_length=255)
     number = models.IntegerField()
-    image = models.ImageField(upload_to='pokemon_images', blank=True, null=True)
-    identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to='pokemon_images/', blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.number}: {self.name}'
+
+class UserPokemon(models.Model):
+    # Represent User's collection of Pokemon
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.pokemon.name}'
